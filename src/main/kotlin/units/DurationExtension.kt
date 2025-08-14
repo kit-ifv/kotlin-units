e1a574cb5c12145b1d77d26e73b41a586b316d2c
zq6966 <zq6966@partner.kit.edu>
2025-08-14 12:58:46 +0200

    changed duration extension documentationi to be a little more clear. Also added Duration times Duration to equal SquareDuration.

--- src/main/kotlin/units/DurationExtension.kt
+++ src/main/kotlin/units/DurationExtension.kt
@@ -5,8 +5,9 @@ import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 
 /**
- * inSeconds and the other functions are marked as shadowing a member. Which is true because inSeconds
- * is part of a deprecated experimental API. Since I don' like the warning the methods are now calles asMinutes
+ * In this package conversion to raw numbers are usually called inSeconds  ('in-X'). Since duration is a native kotlin
+ * unit, this naming is sadly marked as shadowing a member. Which is true because inSeconds  is part of a deprecated
+ * experimental API. Since I don't like the warning, the methods are now called 'asMinutes'
  * and if someone is really annoyed by this naming convention - just add "inSeconds, ..." here
  */
 
@@ -14,3 +15,4 @@ inline val Duration.asMinutes: Double get() = this.toDouble(DurationUnit.MINUTES
 inline val Duration.asSeconds: Double get() = this.toDouble(DurationUnit.SECONDS)
 inline val Duration.asHours: Double get() = this.toDouble(DurationUnit.HOURS)
 
+operator fun Duration.times(other: Duration): SquareDuration = (this.asSeconds * other.asSeconds).squareSeconds
