62267d6beb2282c90ef6a90dec8f097c18864a8d
zq6966 <zq6966@partner.kit.edu>
2025-08-15 12:53:21 +0200

    replaced duration with a Duration wrapper, which handles all functionality required for smooth integration with the `OutOfBoundsUnit` system.

---
+++ src/main/kotlin/units/Duration.kt
@@ -0,0 +1,31 @@
+package units
+
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+
+/**
+ * In this package conversion to raw numbers are usually called inSeconds  ('in-X'). Since duration is a native kotlin
+ * unit, this naming is sadly marked as shadowing a member. Which is true because inSeconds  is part of a deprecated
+ * experimental API. Since I don't like the warning, the methods are now called 'asMinutes'
+ * and if someone is really annoyed by this naming convention - just add "inSeconds, ..." here
+ */
+
+
+/**
+ * Inconveniently Kotlin has a Duration implementation already.
+ */
+class DurationWrapper(val duration: Duration): FlexibleUnit {
+    inline val asMinutes: Double get() = duration.toDouble(DurationUnit.MINUTES)
+    inline val asSeconds: Double get() = duration.toDouble(DurationUnit.SECONDS)
+    inline val asHours: Double get() = duration.toDouble(DurationUnit.HOURS)
+
+
+    operator fun times(other: DurationWrapper): SquareDuration = (this.asSeconds * other.asSeconds).square_seconds
+
+    override fun toOutOfBoundsUnit(): OutOfBoundsUnit {
+        return OutOfBoundsUnit(
+            asSeconds,
+            PhysicsUnit(0,1,0))
+    }
+}
+
