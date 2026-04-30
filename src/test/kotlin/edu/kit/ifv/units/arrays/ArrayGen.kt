8366ece0ba84ed0f58d39f723fbb8954a798750f
Anton <ueida@student.kit.edu>
2026-04-30 14:35:40 +0200

    added better indentation, documentation and a developer note.

--- src/test/kotlin/edu/kit/ifv/units/arrays/ArrayGen.kt
+++ src/test/kotlin/edu/kit/ifv/units/arrays/ArrayGen.kt
@@ -32,14 +32,10 @@ fun prepareFile(dir: Path) {
 
 fun arrayFileContent(type: ArrayTypeDescriptor, writer: BufferedWriter) {
     writer.writeHead(type)
+    writer.writeClassDescription(type)
     writer.writeClassAndConstructors(type)
     writer.writeFunctions(type)
-    writer.newLine()
-    writer.write("""
-    }
-    
-    """.trimIndent()
-    )
+    writer.writeEndOfClass()
     writer.writeExtensionFunctions(type)
 }
 
@@ -49,15 +45,15 @@ fun BufferedWriter.writeHead(type: ArrayTypeDescriptor) {
         package edu.kit.ifv.units.arrays
 
         import edu.kit.ifv.units.${type.className}
-    
         """.trimIndent()
     )
+    newLine()
 }
 
 fun BufferedWriter.writeFunctions(type: ArrayTypeDescriptor) {
     for (function in type.functions) {
-        write(function.print(type.className, type.rawValueType))
         newLine()
+        writeIndented(function.print(type.className, type.rawValueType))
     }
 }
 
@@ -68,11 +64,35 @@ fun BufferedWriter.writeExtensionFunctions(type: ArrayTypeDescriptor) {
     }
 }
 
+fun BufferedWriter.writeClassDescription(type: ArrayTypeDescriptor) {
+    newLine()
+    write("""
+        /**
+        * This class is a typed array for ${type.className}. At runtime, it is converted to a regular java array
+        * of the backing type ${type.rawValueType} (${type.rawValueType}[]).
+        */
+    """.trimIndent()
+    )
+}
+
+fun BufferedWriter.writeEndOfClass() {
+    newLine()
+    write("}")
+    newLine()
+}
+
 fun BufferedWriter.writeClassAndConstructors(type: ArrayTypeDescriptor) {
+    newLine()
     write(
         """
         @JvmInline
         value class ${type.className}Array internal constructor(private val rawValues: ${type.rawValueType}Array) {
+             
+            // Developer-Note: This class was generated automatically and likely will again in the future, you might 
+            // want to edit the code-generation instead of this class specifically. You can find the generating 
+            // main-function in `src/test/edu/kit/ifv/units/arrays/ArrayGen.kt`.
+            // The gradle task `generateArrays` regenerates all Array classes.
+            
             constructor(size: Int): this(${type.rawValueType}Array(size))
             constructor(size: Int, init: (index: Int) -> ${type.className}): this(${type.rawValueType}Array(size) { index -> init(index).rawValue })
             constructor(src: Array<${type.className}>): this(src.map { it.rawValue }.to${type.rawValueType}Array())
@@ -80,4 +100,16 @@ fun BufferedWriter.writeClassAndConstructors(type: ArrayTypeDescriptor) {
             
         """.trimIndent()
     )
+}
+
+/**
+ * Writes all lines of the string indented by `level * oneIndentSpaces` spaces.
+ */
+fun BufferedWriter.writeIndented(content: String, level: Int = 1, oneIndentSpaces: Int = 4) {
+    val indent = " ".repeat(level * oneIndentSpaces)
+    val last = content.lines().size - 1
+    for ( (index, line) in content.lines().mapIndexed { index, line -> index to line }) {
+        write(indent + line)
+        if (index != last) newLine()
+    }
 }
