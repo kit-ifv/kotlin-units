001bc614589c22d27116fb5c704eea5aeaf9fe56
Anton <ueida@student.kit.edu>
2026-05-06 14:50:33 +0200

    checking performance. For large arrays it definitely is the fastest, for smaller a typical Array<UnitType> is faster.

--- src/test/kotlin/edu/kit/ifv/units/arrays/playground.kt
+++ src/test/kotlin/edu/kit/ifv/units/arrays/playground.kt
@@ -1,24 +1,39 @@
 package edu.kit.ifv.units.arrays
 
-import edu.kit.ifv.units.degrees
-import edu.kit.ifv.units.kelvin
-import kotlin.time.Duration.Companion.minutes
+import edu.kit.ifv.units.joule
+import edu.kit.ifv.units.kilowatthours
+import kotlin.system.measureNanoTime
 import kotlin.time.Duration.Companion.nanoseconds
 
 
 fun main() {
-    val arrayNotBox = DurationArray(2) { i ->
-        when (i) {
-            1 -> 4.minutes
-            2 -> 5.nanoseconds
-            else -> 0.minutes
+    val size = 50000000
+    val list = buildList {
+        for (i in 0 until size) {
+            add(i.kilowatthours)
         }
     }
-    for (i in 0..100000) {
-        val temperatureArray = listOf(i.minutes, 23.nanoseconds).toDurationArray()
+    val boxArray = list.toTypedArray()
+    val nonBox = boxArray.toEnergyArray()
 
-        print(temperatureArray.average())
-        print(temperatureArray.mean())
-        require(temperatureArray.mean() == temperatureArray.average()) {"Failed for $i"}
+    var res1 = 0.joule
+    val boxTime = measureNanoTime {
+        res1 = (boxArray.sumOf { it.rawValue }).joule
     }
+    var res2 = 0.joule
+    val nonBoxTime = measureNanoTime {
+        res2 = nonBox.sum()
+    }
+
+    var resList = 0.joule
+    val listTime = measureNanoTime {
+        resList = (list.sumOf { it.rawValue }).joule
+    }
+    println("Result is box: $res1")
+    println("Result non-box: $res2")
+    println("Result list: $resList")
+    println("Result is equal: ${res1 == res2 && res1 == resList}")
+    println("Box took ${boxTime.nanoseconds.inWholeMilliseconds}ms")
+    println("Non-Box took ${nonBoxTime.nanoseconds.inWholeMilliseconds}ms")
+    println("List took ${listTime.nanoseconds.inWholeMilliseconds}ms")
 }
